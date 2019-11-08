30f63d4e74833940228f139ccde24c60ba88752e
Harindu Liyanage <harindu.sul@gmail.com>
2019-11-08 23:02:10 +0530

    import components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { LoginComponent } from './component/login/login.component';
+import { NavBarComponent } from './component/dashboard/nav-bar/nav-bar.component';
+import { SegmentationComponent } from './component/dashboard/content/segmentation/segmentation.component';
+import { ClassificationComponent } from './component/dashboard/content/classification/classification.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    LoginComponent
+    LoginComponent,
+    NavBarComponent,
+    SegmentationComponent,
+    ClassificationComponent
   ],
   imports: [
     BrowserModule,
