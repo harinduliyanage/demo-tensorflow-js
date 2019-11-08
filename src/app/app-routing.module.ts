e8c34daf192c2fb4f82b36b1102e6e6074cfa7f0
Harindu Liyanage <harindu.sul@gmail.com>
2019-11-08 23:02:31 +0530

    added child routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent} from './component/dashboard/dashboard.component';
+import {LoginComponent} from './component/login/login.component';
+import {SegmentationComponent} from './component/dashboard/content/segmentation/segmentation.component';
+import {ClassificationComponent} from './component/dashboard/content/classification/classification.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent, data: {title: 'Login'}},
+  {path: 'dashboard', component: DashboardComponent, data: {title: 'Dashboard'},
+    children: [
+      {path: 'segmentation', component: SegmentationComponent, data: {title: 'Segmentation'}},
+      {path: 'classification', component: ClassificationComponent, data: {title: 'Classification'}}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
