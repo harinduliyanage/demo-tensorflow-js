4e77fc7faa469d3a25c611a5966d2e9b852312b8
Harindu Liyanage <harindu.sul@gmail.com>
2019-11-09 23:34:53 +0530

    object ditection with coco ssd model

--- src/app/component/dashboard/content/object-detection/object-detection.component.ts
+++ src/app/component/dashboard/content/object-detection/object-detection.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import * as cocoSSD from '@tensorflow-models/coco-ssd';
+
 
 @Component({
   selector: 'app-object-detection',
@@ -7,9 +9,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ObjectDetectionComponent implements OnInit {
 
+  @ViewChild('video', {static: true})
+  video: ElementRef<HTMLVideoElement>;
+  @ViewChild('canvas', {static: true})
+  canvas: ElementRef<HTMLCanvasElement>;
+
   constructor() { }
 
   ngOnInit() {
+    this.webcam_start();
+    this.predictWithCocoModel();
   }
 
+  webcam_start() {
+    const videoElements = this.video.nativeElement;
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: {facingMode: 'user', }
+      })
+      .then(stream => {
+        videoElements.srcObject = stream;
+        videoElements.onloadedmetadata = () => {
+          videoElements.play();
+        };
+      });
+  }
+
+  public async predictWithCocoModel() {
+    const model = await cocoSSD.load('lite_mobilenet_v2');
+    this.detectFrame(this.video.nativeElement, model);
+  }
+
+  detectFrame = (video, model) => {
+    model.detect(video).then(predictions => {
+      this.renderPredictions(predictions);
+      requestAnimationFrame(() => {
+        this.detectFrame(video, model);
+      });
+    });
+  }
+  renderPredictions = predictions => {
+    const canvas = this.canvas.nativeElement;
+
+    const ctx = canvas.getContext('2d');
+    canvas.width = 300;
+    canvas.height = 300;
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    // Fonts
+    const font = '16px sans-serif';
+    ctx.font = font;
+    ctx.textBaseline = 'top';
+    ctx.drawImage(this.video.nativeElement, 0, 0, 300, 300);
+    predictions.forEach(prediction => {
+      const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+      const width = prediction.bbox[2];
+      const height = prediction.bbox[3];
+      // Bounding box
+      ctx.strokeStyle = '#00FFFF';
+      ctx.lineWidth = 2;
+      ctx.strokeRect(x, y, width, height);
+      // Label background
+      ctx.fillStyle = '#00FFFF';
+      const textWidth = ctx.measureText(prediction.class).width;
+      const textHeight = parseInt(font, 10); // base 10
+      ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+    });
+    predictions.forEach(prediction => {
+
+      const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+      ctx.fillStyle = '#000000';
+      ctx.fillText(prediction.class, x, y);
+    });
+  }
 }
