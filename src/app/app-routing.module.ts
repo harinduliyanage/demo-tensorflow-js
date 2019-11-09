58515ad97a1355bf94abb58844eea6e0a3fd4bb8
Harindu Liyanage <harindu.sul@gmail.com>
2019-11-09 18:33:35 +0530

    added child route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {DashboardComponent} from './component/dashboard/dashboard.component';
 import {LoginComponent} from './component/login/login.component';
 import {SegmentationComponent} from './component/dashboard/content/segmentation/segmentation.component';
 import {ClassificationComponent} from './component/dashboard/content/classification/classification.component';
+import {ObjectDetectionComponent} from './component/dashboard/content/object-detection/object-detection.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent, data: {title: 'Login'}},
   {path: 'dashboard', component: DashboardComponent, data: {title: 'Dashboard'},
     children: [
       {path: 'segmentation', component: SegmentationComponent, data: {title: 'Segmentation'}},
+      {path: 'object-detection', component: ObjectDetectionComponent, data: {title: 'Object Detection'}},
       {path: 'classification', component: ClassificationComponent, data: {title: 'Classification'}}
     ]
   }
